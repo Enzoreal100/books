0d74e8d3fffee7d0ee2c33bb628694f71282bb18
Enzo Real <140109430+Enzoreal100@users.noreply.github.com>
2024-10-21 09:58:04 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const booksEndpoint = 'https://raw.githubusercontent.com/prof-lucassilva/api-books/main/livros.json';
+    const bookList = document.getElementById('carrossel');
+  
+    fetch(booksEndpoint)
+      .then(response => response.json())
+      .then(books=> {
+        books.forEach(book=> {
+          const bookElement = document.createElement('div');
+          if (book.id == 1){
+            console.log('vasco')
+          }
+          else{
+            bookElement.classList.add('carousel-item');
+            bookElement.innerHTML = `
+              <h3>${book.titulo}</h3>
+              <img src="${book.imagem}" alt="${book.titulo}" width="200px">
+              <p>${book.resumo}</p>
+            `;//caso queria controlar o tamanho da imagem no sass/css, remova a propriedade width="200px"
+            bookList.appendChild(bookElement);
+          }
+        });
+      })
+      .catch(error => {
+        console.error('Erro ao carregar os livros:', error);
+      });
+  });  
